cdbd04a3f49de0744f9f6a8cbad55ac1c1770189
luantran96 <luantran96@yahoo.com>
2018-09-17 15:45:52 -0700

    Implement JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+
+  // if (typeof obj === 'string' || typeof obj === 'number' || typeof obj ){
+  //   return '"' 
+  // }
+
+  if(obj == null) return 'null';
+
+
+  if(Array.isArray(obj)){
+
+    return '[' + obj.map(function(val) {
+
+      return stringifyJSON(val);
+    
+    }).join(',') + ']';
+
+  }else if(obj.constructor === Object){
+
+    for (var key in obj){
+      if(typeof obj[key] === 'function' || obj[key] === undefined){
+        delete obj[key];
+      } 
+    } 
+
+    return '{' + Object.keys(obj).map(function(key) {
+    
+      return stringifyJSON(key) + ':' + stringifyJSON(obj[key]); 
+
+      }).join(',') + '}'; 
+
+  }else {
+
+    if(typeof obj === 'string'){
+
+      return '"' + obj + '"';
+
+      } else return '' + obj;
+
+  }
+  
+  
 };
